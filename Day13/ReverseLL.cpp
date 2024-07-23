c52932b9c05afd4ef38286e7a5a36a4704c63361
Devleena Das <devleena2003@gnmail.com>
2024-07-23 11:25:40 +0530

    Day 13 complete

---
+++ Day13/ReverseLL.cpp
@@ -0,0 +1,16 @@
+ListNode *reverseList(ListNode *head)
+{
+
+    if (head == NULL || head->next == NULL)
+    {
+        return head;
+    }
+
+    ListNode *newHead = reverseList(head->next);
+
+    head->next->next = head;
+
+    head->next = NULL;
+
+    return newHead;
+}
