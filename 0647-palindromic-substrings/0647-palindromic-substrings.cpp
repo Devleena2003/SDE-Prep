b867f5b27672d273f703ecd66714bdb049f5930d
Devleena Das <95465835+Devleena2003@users.noreply.github.com>
2024-11-27 17:53:11 +0530

    Time: 1380 ms (7.63%), Space: 406.5 MB (11.81%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+bool isPalindrome(string s, int i, int j){
+    while(i<j){
+        if(s[i]!=s[j]) return false;
+        i++;
+        j--;
+    }
+    return true;
+}
+    int countSubstrings(string s) {
+      int ans=0;
+      int n=s.length();
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                if(isPalindrome(s,i,j)) ans++;
+            }
+        }
+        return ans;
+    }
+};
