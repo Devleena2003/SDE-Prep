47cc91a0684efb984ebc54c836d6c017d3bf37f1
Devleena Das <95465835+Devleena2003@users.noreply.github.com>
2025-02-17 13:06:50 +0530

    Time: 1 ms (88.19%), Space: 7.8 MB (92.23%) - LeetHub

--- 1160-letter-tile-possibilities/1160-letter-tile-possibilities.cpp
+++ 1160-letter-tile-possibilities/1160-letter-tile-possibilities.cpp
@@ -1,24 +1,32 @@
 class Solution {
 public:
- void backtrack(unordered_map<char, int>& freq, int& count) {
-        for (auto& [ch, num] : freq) {
-            if (num > 0) {
-                count++;
-                num--;
-                backtrack(freq, count);
-                num++;
-            }
-        }
-    }
-    
-    int numTilePossibilities(string tiles) {
-        unordered_map<char, int> freq;
-        for (char ch : tiles) {
-            freq[ch]++;
-        }
+ 
+void backtrack(string& tiles, vector<bool>& visited, int& count) {
+    for (int i = 0; i < tiles.size(); i++) {
+
+        if (visited[i]) continue;
         
-        int count = 0;
-        backtrack(freq, count);
-        return count;
+      
+        if (i > 0 && tiles[i] == tiles[i - 1] && !visited[i - 1]) continue;
+
+  
+        visited[i] = true;
+        count++;
+        
+      
+        backtrack(tiles, visited, count);
+        
+      
+        visited[i] = false;
     }
+}
+
+int numTilePossibilities(string tiles) {
+    sort(tiles.begin(), tiles.end());  
+    vector<bool> visited(tiles.size(), false); 
+    int count = 0;
+    
+    backtrack(tiles, visited, count);
+    return count;
+}
 };
