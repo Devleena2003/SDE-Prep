d2f7cb5459d06d55799f91a99c956ed9f7a44986
Devleena Das <95465835+Devleena2003@users.noreply.github.com>
2025-02-21 11:02:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/First negative in every window of size k/first-negative-in-every-window-of-size-k.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution {
+  public:
+    vector<int> FirstNegativeInteger(vector<int>& arr, int k) {
+        // write code here
+        int n=arr.size();
+        vector<int>res;
+        for(int i=0;i<n-k+1;i++){
+            bool neg=false;
+            for(int j=i;j<i+k;j++){
+                if(arr[j]<0){
+                    res.push_back(arr[j]);
+                    neg=true;
+                    break;
+                }
+            }
+          if(!neg){
+              res.push_back(0);
+           
+          }  
+        }
+       return res; 
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        string ks;
+        getline(cin, ks);
+        int k = stoi(ks);
+        Solution ob;
+        vector<int> ans = ob.FirstNegativeInteger(arr, k);
+        for (auto it : ans) {
+            cout << it << " ";
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
