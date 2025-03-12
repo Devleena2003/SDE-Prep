c71f0e5aa6ab52b55422d15e36adf2beee7c0e0c
Devleena Das <95465835+Devleena2003@users.noreply.github.com>
2025-03-12 13:32:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Min Cost Climbing Stairs/min-cost-climbing-stairs.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+//Back-end complete function Template for C++
+
+class Solution {
+  public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        // Write your code here
+          int n=cost.size();
+        vector<int> mincost(n+1);
+        mincost[0]=0, mincost[1]=0;
+        for(int i=2;i<=n;i++){
+            mincost[i]=min(mincost[i-1]+cost[i-1],mincost[i-2]+cost[i-2]);
+        }
+        return mincost[n];
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+
+    while (t-- > 0) {
+        string str;
+        getline(cin, str);
+
+        stringstream ss(str);
+        vector<int> arr;
+        int num;
+        while (ss >> num) {
+            arr.push_back(num);
+        }
+        Solution sln;
+        int res = sln.minCostClimbingStairs(arr);
+        cout << res << endl;
+        cout << "~" << endl;
+    }
+
+    return 0;
+}
+// } Driver Code Ends
