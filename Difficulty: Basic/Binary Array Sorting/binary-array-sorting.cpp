a7e6a59789bfa63773db68a0d6e452238b6f653e
Devleena Das <95465835+Devleena2003@users.noreply.github.com>
2025-05-20 09:10:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Binary Array Sorting/binary-array-sorting.cpp
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    void binSort(vector<int> &arr) {
+        // code here
+        int n=arr.size();
+        int l=0;
+        int m=0;
+        int h=n-1;
+        while(m<=h){
+            if(arr[m]==0){
+                swap(arr[l],arr[m]);
+                l++;
+                m++;
+            }
+            else if(arr[m]==1) m++;
+            else{
+                swap(arr[m],arr[h]);
+                h--;
+            }
+        }
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t; // Number of test cases
+    cin >> t;
+    cin.ignore(); // Ignore the newline after the integer input
+    while (t--) {
+        vector<int> a;
+        string input;
+
+        // Input format: first number n followed by the array elements
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            a.push_back(num); // Read the array elements from input string
+
+        Solution obj;
+        obj.binSort(a);
+
+        for (int i = 0; i < a.size(); i++) {
+            if (i != 0)
+                cout << " ";
+            cout << a[i];
+        }
+        cout << endl;        // Ensure new line after each test case output
+        cout << "~" << endl; // Ensure new line after each test case output
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
