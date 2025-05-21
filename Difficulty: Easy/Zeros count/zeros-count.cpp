420544732999febfcb22c0824338ed8e4741f24f
Devleena Das <95465835+Devleena2003@users.noreply.github.com>
2025-05-21 08:24:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Zeros count/zeros-count.cpp
@@ -0,0 +1,13 @@
+// User function template for C++
+
+class Solution {
+  public:
+    int countZeroes(int arr[], int n) {
+        // code here
+        int c=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]==0) c++;
+        }
+        return c;
+    }
+};
