4fb1fb336e8f64d83d1361a5eca25889571a9764
Devleena Das <95465835+Devleena2003@users.noreply.github.com>
2025-07-03 22:01:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Substring with K Uniques/longest-substring-with-k-uniques.cpp
+++ Difficulty: Medium/Longest Substring with K Uniques/longest-substring-with-k-uniques.cpp
@@ -1,19 +1,9 @@
-//{ Driver Code Starts
-// Initial template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-// User function template for C++
-
 class Solution {
   public:
     int longestKSubstr(string &s, int k) {
-        // your code here
+        // code here
+        int maxi=-1;
         map<char,int>mp;
-        int maxL=-1;
         int i=0;
         for(int j=0;j<s.length();j++){
             mp[s[j]]++;
@@ -21,30 +11,12 @@ class Solution {
                 mp[s[i]]--;
                 if(mp[s[i]]==0) mp.erase(s[i]);
                 i++;
-                
             }
-           if(mp.size()==k) maxL=max(maxL,j-i+1);
             
+            if(mp.size()==k){
+                maxi=max(maxi,j-i+1);
+            }
         }
-        return maxL;
+        return maxi;
     }
 };
-
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        string s;
-        cin >> s;
-        int k;
-        cin >> k;
-        Solution ob;
-        cout << ob.longestKSubstr(s, k) << endl;
-
-        cout << "~"
-             << "\n";
-    }
-}
-
-// } Driver Code Ends
