c73c10761de033a516489df9e0e61e20e0f64d47
Devleena Das <95465835+Devleena2003@users.noreply.github.com>
2025-07-10 10:23:51 +0530

    Time: 0 ms (100%), Space: 13.4 MB (7.1%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -8,8 +8,8 @@ public:
        int high=m*n-1;
        while(low<=high){
         int mid=low+(high-low)/2;
-        int r=mid/n;
-        int c=mid%n;
+        int r=mid/m;
+        int c=mid%m;
         if(matrix[r][c]==target) return true;
         else if(matrix[r][c]>target)high=mid-1;
         else low=mid+1;
