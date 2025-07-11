2c730172905220baf52b5662cf41fe87f9d8df9e
Devleena Das <95465835+Devleena2003@users.noreply.github.com>
2025-07-11 12:15:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Substring with K Uniques/longest-substring-with-k-uniques.cpp
+++ Difficulty: Medium/Longest Substring with K Uniques/longest-substring-with-k-uniques.cpp
@@ -7,7 +7,7 @@ class Solution {
         int i=0;
         for(int j=0;j<s.length();j++){
             mp[s[j]]++;
-            if(mp.size()>k){
+            while(mp.size()>k){
                 mp[s[i]]--;
                 if(mp[s[i]]==0) mp.erase(s[i]);
                 i++;
