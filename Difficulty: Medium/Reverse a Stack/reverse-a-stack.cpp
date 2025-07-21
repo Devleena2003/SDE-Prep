708785323110a26a8b8d8b88e91055be9c8ec732
Devleena Das <95465835+Devleena2003@users.noreply.github.com>
2025-07-21 18:38:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Reverse a Stack/reverse-a-stack.cpp
+++ Difficulty: Medium/Reverse a Stack/reverse-a-stack.cpp
@@ -1,63 +1,22 @@
-//{ Driver Code Starts
-//Initial Template for C++
+// User function Template for C++
 
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-//User function Template for C++
-
-class Solution{
-public:
-    void Reverse(stack<int> &St){
-       queue<int> q;
-
-    // Move all elements from stack to queue
-    while (!St.empty()) {
-        q.push(St.top());
+class Solution {
+  public:
+  void f(stack<int>&St, int ele){
+      if(St.empty()){
+          St.push(ele);
+          return ;
+      }
+      int top=St.top();
+      St.pop();
+      f(St,ele);
+      St.push(top);
+  }
+    void Reverse(stack<int> &St) {
+        if(St.empty()) return ;
+        int top=St.top();
         St.pop();
-    }
-
-    // Move elements back from queue to stack
-    while (!q.empty()) {
-        St.push(q.front());
-        q.pop();
-    }
+        Reverse(St);
+        f(St,top);
     }
 };
-
-//{ Driver Code Starts.
-
-
-int main(){
-    int T;
-    cin>>T;
-    while(T--){
-        int N;
-        cin>>N;
-        stack<int> St;
-        for(int i=0;i<N;i++){
-            int x;
-            cin>>x;
-            St.push(x);
-        }
-        Solution ob;
-        ob.Reverse(St);
-        vector<int>res;
-        while(St.size())
-        {
-            res.push_back(St.top());
-            St.pop();
-        }
-        for(int i = res.size()-1;i>=0;i--)
-        {
-            cout<<res[i]<<" ";
-        }
-        
-        cout<<endl;
-    
-cout << "~" << "\n";
-}
-}
-// } Driver Code Ends
