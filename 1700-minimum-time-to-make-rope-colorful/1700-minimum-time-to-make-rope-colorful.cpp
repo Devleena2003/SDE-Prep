6e5af482fab15cfb27e102fbe65f862c0807673b
Devleena Das <95465835+Devleena2003@users.noreply.github.com>
2025-08-18 11:05:52 +0530

    Time: 3 ms (71.06%), Space: 99.7 MB (98.99%) - LeetHub

---
+++ 1700-minimum-time-to-make-rope-colorful/1700-minimum-time-to-make-rope-colorful.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minCost(string colors, vector<int>& neededTime) {
+        int n=neededTime.size();
+        int time=0;
+        int prev=0;
+        for(int i=0;i<n;i++){
+            if(i>0 && colors[i]!=colors[i-1]) prev=0;
+            int curr=neededTime[i];
+            time+=min(curr,prev);
+            prev=max(prev,curr);
+
+        }
+        return time;
+    }
+};
