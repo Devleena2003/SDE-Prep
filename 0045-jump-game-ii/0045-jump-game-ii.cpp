87dbf96fa05250e9f5c1b86415f7abfd6d62bf75
Devleena Das <95465835+Devleena2003@users.noreply.github.com>
2025-09-11 07:59:57 +0530

    Time: 0 ms (100%), Space: 20.5 MB (63%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-       for (int i=1;i<nums.size();i++){
-        nums[i]=max(nums[i]+i, nums[i-1]);  //rebuild the nums array
-        //[2,3,1,1,4]
-        // [2, 4, 4, 4, 8]
-       } 
-       int index,ans=0;
-       while(index<nums.size()-1){
-        ans++;
-        index=nums[index];
+     int j=0,pos=0,dest=0;
+     for(int i=0;i<nums.size()-1;i++){
+       dest=max(dest,i+nums[i]);
+       if(pos==i){
+        pos=dest;
+        j++;
        }
-       return ans;
+       if(pos>=(nums.size()-1))return j;
+
+     }
+     return 0;
+
     }
 };
