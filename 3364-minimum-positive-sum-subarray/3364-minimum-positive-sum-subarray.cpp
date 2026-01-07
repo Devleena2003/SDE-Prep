a53526f883be62d03aeb0354095a7bebd70caef9
Devleena Das <95465835+Devleena2003@users.noreply.github.com>
2026-01-07 19:16:11 +0530

    Time: 4 ms (24.78%), Space: 30.5 MB (12.17%) - LeetHub

--- 3364-minimum-positive-sum-subarray/3364-minimum-positive-sum-subarray.cpp
+++ 3364-minimum-positive-sum-subarray/3364-minimum-positive-sum-subarray.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     int minimumSumSubarray(vector<int>& nums, int l, int r) {
-        int sum=0;
+        int n=nums.size();
+        vector<int>prefix(n+1,0);
+        for(int i=0;i<nums.size();i++){
+            prefix[i+1]=nums[i]+prefix[i];
+        }
         int minSum=INT_MAX;
-      for(int i=0;i<nums.size();i++){
-        sum=0;
-        for(int j=i;j<nums.size();j++){
-            sum+=nums[j];
-            int len=j-i+1;
-            if(len>=l && len<=r && sum>0){
-                minSum=min(sum,minSum);
+        for(int i=l;i<=r;i++){
+            for(int j=0;j+i<=n;j++){
+             int sum=prefix[j+i]-prefix[j];
+             if(sum>0){
+                minSum=min(minSum,sum);
+
+             }
             }
         }
-      }
-      return minSum==INT_MAX?-1:minSum;
+        return minSum==INT_MAX?-1:minSum;
     }
 };
